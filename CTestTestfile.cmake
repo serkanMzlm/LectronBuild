3296566245f1c8cdad6b8e92cafbaaf4ee7fa9b3
serkan <serkanmazlum306@gmail.com>
2024-10-15 15:11:54 +0300

    dosyalar silindi

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ems/Lectron
-# Build directory: C:/ems/Lectron/build/Desktop_Qt_5_15_2_MSVC2019_64bit-Release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
